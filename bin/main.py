012075f10f747d2fc7a57cdc70ed0fa68a3fa09e
Kwangjin1986 <kpa35@sfu.ca>
2015-08-13 01:32:38 -0700

    main

--- bin/main.py
+++ bin/main.py
@@ -5,64 +5,85 @@ def main():
 	import matplotlib.pyplot as plt
 	
 	##data regular expression
-	datatable = regularexpression(sys.argv[1])
+	ttable = regularexpression(sys.argv[1])
 
 	## export revised table to newfile
-	datatable.to_csv("newmerged.file", sep='\t')
+	#ttable.to_csv("newmerged.file", sep='\t')
 	
-	#Figures
-	#bodysizeA(datatable)
-	#bodysizeB(datatable)
-	#plt.show()
+	##cut table from 530 - 590
+	modified_table = cuttable(ttable)
+	#modified_table.to_csv("cuttable.file", sep='\t')
+	
+	#Figures_bodysize
+	#bodysize_bar(modified_table)
+	bodysize_box(modified_table)
+	
+	#Figure_length
+	#bodylength_bar(modified_table)
+	bodylength_box(modified_table)
+	
+	
 
 	
 # regularexpression function
 def regularexpression(data):
 	
-	## If file size is small, it is ok to use *re* library.
-	## *regex* is a faster regular expression library than *re* library.
-	## To use regex library, download and install the library. 
-	import regex
+	
+	import re
 	
 	##pandas is dataframe library
 	import pandas as pd
 	
 	## read a file as table
-	gDat = pd.read_table(data, sep="\s+", names = ['v1',"date", "plate", "time", "strain", "frame", "ID", "persistance", "area", "speed", "angularspeed", "length", "rellength", "width", "relwidth", "aspect", "relaspect", "midline", "morphwidth", "kink", "bias", "pathlen", "curve", "dir", "loc_x", "loc_y", "vel_x", "vel_y", "orient", "crab"])
+	datatable = pd.read_table(data, sep="\s+", names = ['v1', "FRAME", "ID", "PERSISTANCE", "AREA", "SPEED", "ANGULARSPEED", "LENGTH", "RELLENGTH", "WIDTH", "RELWIDTH", "ASPECT", "RELASPECT", "MIDLINE", "MORPHWIDTH", "KINK", "BIAS", "PATHLEN", "CURVE", "DIR", "loc_x", "loc_y", "vel_x", "vel_y", "ORIENT", "CRAB", "TAP", "PUFF", "STIM3", "STIM4"])
 	
 	
 	## delete column(v1) from the table
-	newgDat = gDat.drop('v1', axis=1)
+	revised_datatable = datatable.drop(['v1',"TAP", "PUFF", "STIM3", "STIM4"], axis=1)
 	
 	## compile a regular expression pattern. It will speed up the ""findall"" function up to 100 times.
 	## open a file as strings: This process is necessary for the ""regex.findall"" function.
 	lines = []
 	lines = open(data).read()
 	
-	PA = regex.compile('./([0-9]{8})_')
-	PB = regex.compile('./([0-9]{8}_[0-9]{6})/')
-	PC = regex.compile('/([A-Za-z]+[-]?[0-9]+)')
-	N = regex.compile(':([0-9]+[.][0-9]+)')
-	Date = PA.findall(lines)
-	Plate = PB.findall(lines)
-	Strain = PC.findall(lines)
-	Time = N.findall(lines)
+	comDATE = re.compile('./([0-9]{8})_')
+	comPLATE = re.compile('./([0-9]{8}_[0-9]{6})/')
+	comSTRAIN = re.compile('/([A-Za-z]+[-]?[0-9]+)')
+	comTIME = re.compile(':([0-9]+[.][0-9]+)')
+	Date = comDATE.findall(lines)
+	Plate = comPLATE.findall(lines)
+	Strain = comSTRAIN.findall(lines)
+	Time = comTIME.findall(lines)
 	table = {'DATE' : Date, 'PLATE' : Plate, 'TIME' : Time, 'STRAIN' : Strain}
 	df = pd.DataFrame(table)
 	df = df[['DATE', 'PLATE', 'TIME', 'STRAIN']]
 	
-	## To save memory and avoid to recall recent data
+	## To save memory
 	del lines
-	del gDat
+	del datatable
 	
 	## merge two tables to one table
-	Newdata = pd.merge(df, newgDat, left_index=True, right_index=True, how='outer')
+	Newdata = pd.merge(df, revised_datatable, left_index=True, right_index=True, how='outer')
 	return Newdata
 	
-def bodysizeA(table):
+	
+def cuttable(info):
+    
+	import pandas as pd
+	import numpy as np
+	#newtable = pd.read_table(info, index_col=0)
+	#a_newtable = newtable[newtable.TIME <= 590]
+	info['TIME'] = info['TIME'].astype('float64')
+	a_newtable = info[info.TIME <= 590]
+	b_newtable = a_newtable[a_newtable.TIME >= 530]
+	sort_table = b_newtable.sort(["STRAIN", "PLATE", "ID"])
+	return sort_table
+	
+def bodysize_bar(dataset):
 	import matplotlib.pyplot as plt
 	import numpy as np
-	sizetable = table.groupby('STRAIN')
+	
+	sizetable = dataset.groupby('STRAIN')
 	
 	#plt.boxplot(sizetable['AREA'])
 	sizevalue = sizetable['AREA'].aggregate(np.mean)
@@ -70,14 +91,34 @@ def bodysizeA(table):
 	my_plot = sizevalue.plot(kind='bar', yerr=std, color='r')
 	my_plot.set_xlabel('Strain')
 	my_plot.set_ylabel('Body size (um)')
-	return my_plot
+	return plt.savefig('Bodysize_bar.png', bbox_inches='tight')
 	
-def bodysizeB(box):
+def bodysize_box(dataSet):
+	import matplotlib.pyplot as plt
+	Sizetable = dataSet.groupby('STRAIN')
+	Sizetable.boxplot(column = 'AREA', showfliers=False, showmeans = True, return_type = 'axes')
+	return plt.savefig('Bodisize_boxplot.png', bbox_inches='tight')
+	
+	
+def bodylength_bar(box):
 	#import matplotlib.pyplot as plt
-	sizetable = box.groupby('STRAIN')
-	myplot = sizetable.boxplot(column = 'AREA', showfliers=False, showmeans = True, return_type = 'axes')
-	return myplot
+	import matplotlib.pyplot as plt
+	import numpy as np
+	length_table = box.groupby('STRAIN')
 	
+	#plt.boxplot(sizetable['AREA'])
+	lengthvalue = length_table['LENGTH'].aggregate(np.mean)
+	std = length_table['LENGTH'].aggregate(np.std)
+	myplot = lengthvalue.plot(kind='bar', yerr=std )
+	myplot.set_xlabel('Strain')
+	myplot.set_ylabel('Body length (um)')
+	return plt.savefig('length_bar.png', bbox_inches='tight')
+
+def bodylength_box(Box):
+	import matplotlib.pyplot as plt
+	Length_table = Box.groupby('STRAIN')
+	Length_table.boxplot(column = 'LENGTH', showfliers=False, showmeans = True, return_type = 'axes')
+	return plt.savefig('length_boxplot.png', bbox_inches='tight')
 
 if __name__ == '__main__':
 	
